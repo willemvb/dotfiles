92e39a005fe2290b2a070b14886a57910de6e370
Willem Van Bockstal <willem@spatie.be>
2020-05-06 11:29:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,8 +14,7 @@ rm -rf $HOME/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Symlink zsh prefs
-rm $HOME/.zshrc
-ln -s ./cli/.zshrc $HOME/.zshrc
+ln -s -f $PWD/cli/.zshrc $HOME/.zshrc
 
 # Install Spaceship theme
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
@@ -23,22 +22,11 @@ ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/the
 
 # Symlink iTerm2 settings
 echo 'Install iTerm prefs'
-rm $HOME/Library/Preferences/com.googlecode.iterm2.plist
-ln -s ./cli/com.googlecode.iterm2.plist $HOME/Library/Preferences/com.googlecode.iterm2.plist
+ln -s -f $PWD/cli/com.googlecode.iterm2.plist $HOME/Library/Preferences/com.googlecode.iterm2.plist
 
 # Activate z
 echo 'Install z'
 chmod +x ./cli/z.sh
-
-# Download fonts
-echo 'Install IBM Plex'
-wget "https://github.com/IBM/type/archive/master.zip"
-unzip master.zip
-rm master.zip
-cp ./plex-master/IBM-Plex-Mono/**/*.otf $HOME/Library/fonts/
-cp ./plex-master/IBM-Plex-Sans/**/*.otf $HOME/Library/fonts/
-cp ./plex-master/IBM-Plex-Serif/**/*.otf $HOME/Library/fonts/
-rm -rf plex-master
 
 ###############################################################################
 # Git config                                                                  #
@@ -46,18 +34,10 @@ rm -rf plex-master
 
 echo 'Install git preferences'
 # Add global gitignore
-ln -s ./git/.global-gitignore $HOME/.global-gitignore
+ln -s -f $PWD/git/.global-gitignore $HOME/.global-gitignore
 
 # Add gitconfig
-ln -s ./git/.gitconfig $HOME/.gitconfig
-
-###############################################################################
-# Miscellaneous                                                               #
-###############################################################################
-
-echo 'Install other preferences'
-# Add global gitignore
-ln -s ./misc/.ignition $HOME/.ignition
+ln -s -f $PWD/git/.gitconfig $HOME/.gitconfig
 
 ###############################################################################
 # Install Brew packages                                                       #
@@ -69,13 +49,13 @@ brew tap homebrew/services
 brew doctor
 
 echo 'Install wget'
-brew install wget --overwrite
+brew install wget
 
 echo 'Install PHP versions'
-brew install php@7.0
-brew install php@7.1
 brew install php@7.2
+brew install php@7.3
 brew install php
+brew services start php
 
 echo 'Install ImageMagick'
 brew install imagemagick
@@ -105,25 +85,30 @@ echo 'Install Redis'
 pecl install redis
 
 ###############################################################################
+# Miscellaneous                                                               #
+###############################################################################
+
+echo 'Install other preferences'
+# Add ignition
+ln -s -f $PWD/misc/.ignition $HOME/.ignition
+
+# Download fonts
+echo 'Install IBM Plex'
+wget "https://github.com/IBM/type/archive/master.zip"
+unzip master.zip
+rm master.zip
+cp ./plex-master/IBM-Plex-Mono/**/*.otf $HOME/Library/fonts/
+cp ./plex-master/IBM-Plex-Sans/**/*.otf $HOME/Library/fonts/
+cp ./plex-master/IBM-Plex-Serif/**/*.otf $HOME/Library/fonts/
+rm -rf plex-master
+
+
+###############################################################################
 # Install Composer                                                            #
 ###############################################################################
 
 echo 'Install Composer'
-EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
-
-if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
-then
-    >&2 echo 'ERROR: Invalid installer checksum'
-    rm composer-setup.php
-    exit 1
-fi
-
-php composer-setup.php --quiet
-RESULT=$?
-rm composer-setup.php
-mv composer.phar /usr/local/bin/composer
+brew install composer
 
 echo 'Install Laravel Envoy'
 composer global require laravel/envoy
@@ -135,3 +120,5 @@ mkdir $HOME/Sites
 cd $HOME/Sites
 valet park
 cd $HOME
+
+echo '😎 Done!'
